fdd775343c7e95c459ab4ba30ea3dff5b5ad3ebd
saksham1520 <saksham1520@gmail.com>
2024-12-04 18:14:39 +0530

    Updated UI and fixed API bugs

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   return (
     <>
       <HeroSection />
-      {user.role === "student" && <CategoryCarousel />}
+      {user?.role === "student" && <CategoryCarousel />}
       <CategoryCarousel />
       <LatestJobs />
     </>
