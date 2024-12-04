b97606099cefecd039f97ae03b403057811f517e
saksham1520 <saksham1520@gmail.com>
2024-12-04 18:10:49 +0530

    Updated UI and fixed API bugs

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -3,13 +3,15 @@ import HeroSection from "./HeroSection";
 import CategoryCarousel from "./CategoryCarousel";
 import LatestJobs from "./LatestJobs";
 import useGetAllJobs from "@/hooks/useGetAllJobs";
+import { useSelector } from "react-redux";
 
 function Home() {
   useGetAllJobs();
-
+  const { user } = useSelector((store) => store.auth);
   return (
     <>
       <HeroSection />
+      {user.role === "student" && <CategoryCarousel />}
       <CategoryCarousel />
       <LatestJobs />
     </>
